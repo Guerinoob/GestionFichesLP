d9d06ec7142ecc4d85c690cc9f9ade8d1ba74610
DarkZerba <zebra72000@gmail.com>
2019-04-13 17:38:39 +0200

    MERCI JEREMY

---
+++ src/main/java/com/gestionficheslp/GestionFichesLpApplication.java
@@ -0,0 +1,13 @@
+package com.gestionficheslp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFichesLpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFichesLpApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gestionficheslp/controllers/LPController.java
@@ -0,0 +1,16 @@
+package com.gestionficheslp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LPController {
+
+	
+	@RequestMapping("/index")
+	public String index(ModelMap model) {
+		model.addAttribute("message", "BONJOUR ADRIEN !");
+		return "index";
+	}
+}
